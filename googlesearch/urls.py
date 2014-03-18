5a77ee98f17c1db9bf24813586b411b2500b9f8a
Anurag <anssharma61@gmail.com>
2014-03-19 04:41:32 +0530

    made the app compatible

--- googlesearch/urls.py
+++ googlesearch/urls.py
@@ -10,16 +10,29 @@ urlpatterns = patterns(
     '',
 
     url(
-        r'^results/$', 
-        'django.views.generic.simple.direct_to_template', 
-        {'template': 'googlesearch/results.html'},
-        name='googlesearch-results'
-    ),
-
-    url(
-        r'^cref-cse\.xml/$', 
-        'googlesearch.views.cref_cse', 
+        r'^cref-cse\.xml/$',
+        'googlesearch.views.cref_cse',
         {},
         name='googlesearch-cref-cse'
     ),
 )
+if version <= 1.4:
+    urlpatterns += patterns(
+        '',
+        url(
+            r'^results/$',
+            'django.views.generic.simple.direct_to_template',
+            {'template': 'googlesearch/results.html'},
+            name='googlesearch-results'
+        ),
+    )
+else:
+    from django.views.generic import TemplateView
+    urlpatterns += patterns(
+        '',
+        url(
+            r'^results/$',
+            TemplateView.as_view(template_name='googlesearch/results.html'),
+            name='googlesearch-results'
+        ),
+    )
--- googlesearch/views.py
+++ googlesearch/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext, loader
 
 def cref_cse(request):
     return HttpResponse(
-        loader.render_to_string('googlesearch/cref_cse.xml', {}, context_instance=RequestContext(request)),
-        mimetype = 'text/xml'
+        loader.render_to_string('googlesearch/cref_cse.xml', {},
+                                context_instance=RequestContext(request)),
+        mimetype='text/xml'
     )
